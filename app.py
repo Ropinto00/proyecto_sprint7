831058b2aad9d32945e6bd29223ea74298769abd
Ropinto00 <ropinto0706@gmail.com>
2025-10-22 22:19:37 -0600

    Titulo

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-car_data = pd.read_csv('vehicles_us.csv')
-hist_button = st.button('Construir histograma')
+st.markdown('# **App de Sprint 7: Roberto Pinto**')
 
-st.markdown('**App de Sprint 7: Roberto Pinto**')
+ar_data = pd.read_csv('vehicles_us.csv')
+hist_button = st.button('Construir histograma')
 
 if hist_button:
     st.write(
