1fb19120ed7f8fef39a6056659809fda7407ff85
Ropinto00 <ropinto0706@gmail.com>
2025-10-22 21:05:34 -0600

    Primera versión

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # proyecto_sprint7
-Repositorio para el proyecto del sprint 7 de
+Repositorio para el proyecto del sprint 7. En este proyecto se va a construir una aplicación que pueda generar 2 gráficos: El primero es un histograma con la distribución del kilometraje de los coches; el segundo es un gráfico de disperción entre el precio y el kilometraje de los coches en USA.
---
+++ app.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles_us.csv')
+hist_button = st.button('Construir histograma')
+
+if hist_button:
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig_1 = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig_1, use_container_width=True)
+
+disp_button = st.button('Construir un gráfico de dispersión')
+
+if disp_button:
+    st.write("Creación de un gráfico de dispersión entre precio y kilometraje")
+    fig_2 = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig_2, use_container_width=True)
